3ba22097d45d8703ca68d7f41402c0ed16d35b6a
sreetama54 <sreetamamitra54@gmail.com>
2024-10-27 13:41:22 +0000

    temp commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,21 @@ import "./index.css";
 import "./styles.css";
 import Home from "./Home";
 
+async function getData() {
+  const url = "https://s3.eu-west-1.amazonaws.com/hackajob-assets1.p.hackajob/challenges/football_session/football.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+ 
 const App = () => (
   <div className="container">
     <div><Home/></div>
